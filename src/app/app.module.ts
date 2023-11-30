95879f8967b26841a2894225db9af94223ace5cf
ibrian91 <ibrian.giorgio@gmail.com>
2023-11-30 16:11:43 -0300

    games-list funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GamesListComponent } from './modules/games/components/games-list/games-list.component';
+import { GamesFormComponent } from './modules/games/components/games-form/games-form.component';
+import { CreateGamesComponent } from './modules/games/pages/create-games/create-games.component';
+import { EditGamesComponent } from './modules/games/pages/edit-games/edit-games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesListComponent,
+    GamesFormComponent,
+    CreateGamesComponent,
+    EditGamesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modules/games/pages/edit-games/edit-games.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-games',
+  templateUrl: './edit-games.component.html',
+  styleUrls: ['./edit-games.component.css']
+})
+export class EditGamesComponent {
+
+}
